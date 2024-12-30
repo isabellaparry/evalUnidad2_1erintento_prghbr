5d2256c007036c041bd435d9654809e077cb3881
iparry <iparry@achs.cl>
2024-12-30 11:57:32 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'evalU2prghbrd',
+  webDir: 'www'
+};
+
+export default config;
